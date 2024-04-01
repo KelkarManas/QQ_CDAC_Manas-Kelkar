3af1f2300c64e48fd308142b8ccaf7abfb5c881e
kd1-80098 <manaskelkar42@gmail.com>
2024-04-01 23:30:45 +0530

    added chat-app frontend and socket API backend

---
+++ server/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
+});
+
+server.listen(3001, () => {
+  console.log('Server running on port 3001');
+});
